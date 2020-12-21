3035ea8746afdb6159c6f365d67226116ec225cf
Peeraphtw <judiciarypz@gmail.com>
2020-12-21 17:03:03 +0700

    mapredux

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 class SongList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderSongList = this.renderSongList.bind(this);
+  }
+  renderSongList() {
+    return this.props.song.map((item) => {
+      return (
+        <div key={item.duration}>
+          <h3 className='ui header'>{item.title}</h3>
+          <h4 className='ui'>{item.duration}</h4>
+        </div>
+      );
+    });
+  }
   render() {
-    console.log(this.props);
-    return <div>SongList</div>;
+    console.log(this.props.song);
+
+    return <div>{this.renderSongList()}</div>;
   }
 }
 const mapStateToProps = (state) => {
