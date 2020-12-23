6f0792978fd2e71540a3b7ed9d9c8b3b6efe0bdc
Peeraphtw <judiciarypz@gmail.com>
2020-12-23 11:12:49 +0700

    success

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -24,7 +24,6 @@ class SongList extends React.Component {
     });
   }
   render() {
-    console.log(this.props);
     return <div className='ui list divided'>{this.renderSongList()}</div>;
   }
 }
